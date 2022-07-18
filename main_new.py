cbe8bf3a4374f51ebf57e081bd3dad959252655b
LiGuiye <guiyli@ttu.edu>
2022-07-17 18:16:39 -0600

    add solar dataset training and time consuming save

--- main_new.py
+++ main_new.py
@@ -1,50 +1,10 @@
 from PhIREGANs import *
 
-# # WIND Total Scale: 50X
-# data_type = 'wind'
-# batch_size = 4
-# # -------------------------------------------------------------
-# # LR-MR (10, 10, 2) --> (100, 100, 2)
-# data_path = 'example_data/wind_2007,2008_LR-MR-train.tfrecord'
-# model_path = None # (string) path of previously trained model to load in if continuing training
-# r = [2, 5]
-# mu_sig = [[-1.74402694, -1.74402694], [7.18036716, 7.18036716]]
-# -------------------------------------------------------------
-# MR-HR (100, 100, 2) --> (500, 500, 2)
-# data_path = 'example_data/wind_2007,2008_MR-HR-train.tfrecord'
-# model_path = None
-# r_mr_hr = [5]
-# mu_sig = [[-0.65514054, -1.0888864], [5.75660848, 4.80301305]]
 
-
-# SOLAR - LR-MR
-# -------------------------------------------------------------
-# LR-MR (20, 20, 2) --> (100, 100, 2)
-# MR-HR (100, 100, 2) --> (500, 500, 2)
-# '''
-# data_type = 'solar'
-# data_path = 'example_data/solar_LR-MR.tfrecord'
-# model_path = 'models/solar_lr-mr/trained_gan/gan'
-# r = [5]
-# [[mean_],[]]
-# mu_sig=[[344.3262, 113.7444], [370.8409, 111.1224]]
-# '''
-
-# SOLAR - MR-HR
-# -------------------------------------------------------------
-# '''
-# data_type = 'solar'
-# data_path = 'example_data/solar_MR-HR.tfrecord'
-# model_path = 'models/solar_mr-hr/trained_gan/gan'
-# r = [5]
-# mu_sig = [[344.3262, 113.7444], [386.9283, 117.9627]]
-# '''
-
-if __name__ == '__main__':
+def train_Wind(batch_size = 8, epoch = 10):
     # WIND Total Scale: 50X
     data_type = 'wind'
-    batch_size = 4
-    epoch = 10
+
     # -------------------------------------------------------------
     # LR-MR (10, 10, 2) --> (100, 100, 2)
     data_path = 'example_data/wind_2007,2008_LR-MR-train.tfrecord'
@@ -74,3 +34,51 @@ if __name__ == '__main__':
     model_dir = phiregans.train(
         r=r, data_path=data_path, model_path=model_dir, batch_size=batch_size
     )
+
+def train_Solar(batch_size = 8, epoch = 10):
+    # WIND Total Scale: 25X
+    data_type = 'solar'
+
+    # -------------------------------------------------------------
+    # LR-MR (20, 20, 2) --> (100, 100, 2)
+    data_path = 'example_data/solar_2009,2010,2011_LR-MR-train.tfrecord'
+    model_path = None
+    r = [5]
+    phiregans = PhIREGANs(data_type=data_type, mu_sig=None, print_every=400, N_epochs=epoch)
+    # train cnn
+    model_dir = phiregans.pretrain(
+        r=r, data_path=data_path, model_path=model_path, batch_size=batch_size
+    )
+    # train gan
+    model_dir = phiregans.train(
+        r=r, data_path=data_path, model_path=model_dir, batch_size=batch_size
+    )
+
+    # -------------------------------------------------------------
+    # MR-HR (100, 100, 2) --> (500, 500, 2)
+    data_path = 'example_data/wind_2007,2008_MR-HR-train.tfrecord'
+    model_path = None
+    r = [5]
+    phiregans = PhIREGANs(data_type=data_type, mu_sig=None, print_every=400, N_epochs=epoch)
+    # train cnn
+    model_dir = phiregans.pretrain(
+        r=r, data_path=data_path, model_path=model_path, batch_size=batch_size
+    )
+    # train gan
+    model_dir = phiregans.train(
+        r=r, data_path=data_path, model_path=model_dir, batch_size=batch_size
+    )
+    return model_dir
+if __name__ == '__main__':
+    start_time = time.time()
+
+    model_dir = train_Solar(batch_size = 8, epoch = 10)
+
+    finish_time = time.time() - start_time
+    total_time_print = 'Training Finished. Took {:.4f} minutes or {:.4f} hours to complete.'.format(
+        finish_time / 60, finish_time / 3600
+    )
+    print(total_time_print)
+    text_file = open(os.path.join(model_dir, "total_time.txt"), "w")
+    text_file.write(total_time_print)
+    text_file.close()
