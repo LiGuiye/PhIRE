0a974ac6d9ec7f1ec50acaf6d5142ea86d7f0e14
LiGuiye <1171116470@qq.com>
2022-08-13 21:17:34 -0500

    add hpcc dataset path for solar add args parser

--- main_new.py
+++ main_new.py
@@ -1,5 +1,7 @@
+import argparse
+from time import time
+
 from PhIREGANs import *
-import time
 
 
 def train(
@@ -7,7 +9,11 @@ def train(
 ):
     # -------------------------------------------------------------
     # LR-MR  solar (20, 20, 2) wind (10, 10, 2) --> (100, 100, 2)
-    data_path = 'example_data/' + dataset_name + '_LR-MR-train.tfrecord'
+    if dataset_name == "solar_2007,2008,2009,2010,2011,2012":
+        # this one is too large
+        data_path = '/lustre/scratch/guiyli/Dataset_NSRDB/PhIRE/solar_2007,2008,2009,2010,2011,2012_LR-MR-train.tfrecord'
+    else:
+        data_path = 'example_data/' + dataset_name + '_LR-MR-train.tfrecord'
     model_path = None
     r = [5] if data_type == 'solar' else [2, 5]
     phiregans = PhIREGANs(
@@ -24,7 +30,11 @@ def train(
 
     # -------------------------------------------------------------
     # MR-HR (100, 100, 2) --> (500, 500, 2)
-    data_path = 'example_data/' + dataset_name + '_MR-HR-train.tfrecord'
+    if dataset_name == "solar_2007,2008,2009,2010,2011,2012":
+        # this one is too large
+        data_path = '/lustre/scratch/guiyli/Dataset_NSRDB/PhIRE/solar_2007,2008,2009,2010,2011,2012_MR-HR-train.tfrecord'
+    else:
+        data_path = 'example_data/' + dataset_name + '_MR-HR-train.tfrecord'
     model_path = None
     r = [5]
     phiregans = PhIREGANs(
@@ -42,22 +52,33 @@ def train(
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    # -----------------------------------------------
+    parser = argparse.ArgumentParser(description='LAG-Pytorch')
+    parser.add_argument('--data_type', type=str, default='solar', help='solar or wind')
+    parser.add_argument('--dataset_name', type=str, default='solar_2009', help='solar_2007,2008,2009,2010,2011,2012 or solar_2009 or wind_2007,2008')
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--epoch', type=int, default=10)
+    # parse config
+    args = parser.parse_args()
 
+    start_time = time()
+    # -----------------------------------------------
     # model_dir = train(
-    #     data_type='solar', dataset_name='solar_2009,2010,2011', batch_size=8, epoch=10
+        # data_type='solar', dataset_name='solar_2007,2008,2009,2010,2011,2012', batch_size=16, epoch=10
     # )
 
     # model_dir = train(
     #     data_type='wind', dataset_name='wind_2007,2008', batch_size=8, epoch=10
     # )
 
+    # model_dir = train(
+    #     data_type='solar', dataset_name='solar_2009', batch_size=16, epoch=10
+    # )
+
     model_dir = train(
-        data_type='solar', dataset_name='solar_2009', batch_size=8, epoch=10
+        data_type=args.data_type, dataset_name=args.dataset_name, batch_size=args.batch_size, epoch=args.epoch
     )
     # -----------------------------------------------
-    finish_time = time.time() - start_time
+    finish_time = time() - start_time
     total_time_print = 'Training Finished. Took {:.4f} minutes or {:.4f} hours to complete.'.format(
         finish_time / 60, finish_time / 3600
     )
